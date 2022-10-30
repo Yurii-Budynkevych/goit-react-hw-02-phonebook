3c1cea6536a89f889de971af0fc9f4bbd8b48c2f
Yurii-Budynkevych <yurii.budynkevych@gmail.com>
2022-10-31 00:59:06 +0200

    form add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,26 @@
-export const App = () => {
-  return <div>Hello</div>;
-};
+import React from 'react';
+import shortid from 'shortid';
+import ContactForm from './Form/Form';
+
+export class App extends React.Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  submitHandler = (values, { resetForm }) => {
+    values.id = shortid.generate();
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, values],
+    }));
+    resetForm();
+  };
+
+  render() {
+    return (
+      <>
+        <ContactForm onSubmit={this.submitHandler} />
+      </>
+    );
+  }
+}
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,41 @@
+import { Formik, Form, Field } from 'formik';
+import './Form.css';
+
+const init = {
+  name: '',
+  number: '',
+};
+
+const ContactForm = ({ onSubmit }) => {
+  return (
+    <>
+      <h1 className="title">Phonebook</h1>
+      <Formik initialValues={init} onSubmit={onSubmit}>
+        <Form autoComplete="off">
+          <label>
+            Name
+            <Field
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label>
+            Number{' '}
+            <Field
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="sumbit">Add contact</button>
+        </Form>
+      </Formik>
+    </>
+  );
+};
+export default ContactForm;
